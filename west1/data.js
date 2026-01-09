fed31aa12940ee3a83f36336818362498c4e3ce8
11430147-user <11430147@sssh.tp.edu.tw>
2026-01-09 12:06:50 +0800

    west1

---
+++ west1/data.js
@@ -0,0 +1,226 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-street-view--west1",
+      "name": "Street View  west1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.601011082662433,
+        "pitch": 0.21778436216906272,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.364335499002408,
+          "pitch": 0.21868336401567134,
+          "rotation": 4.71238898038469,
+          "target": "1-street-view-west-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-street-view-west-2",
+      "name": "Street View west 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5901700263992664,
+        "pitch": 0.08768902583028293,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5242749423953992,
+          "pitch": 0.03262257031924598,
+          "rotation": 7.853981633974483,
+          "target": "0-street-view--west1"
+        },
+        {
+          "yaw": 1.3730150917827135,
+          "pitch": 0.02816672486167704,
+          "rotation": 4.71238898038469,
+          "target": "2-street-view-west-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7914055128960946,
+          "pitch": 0.24358275934891438,
+          "title": "地下街",
+          "text": "底下有算命"
+        }
+      ]
+    },
+    {
+      "id": "2-street-view-west-3",
+      "name": "Street View west 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5262726213868811,
+        "pitch": -0.09761780749683169,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.555609172643436,
+          "pitch": 0.2690609259436556,
+          "rotation": 1.5707963267948966,
+          "target": "1-street-view-west-2"
+        },
+        {
+          "yaw": 1.3744305329640536,
+          "pitch": 0.2619578613480513,
+          "rotation": 4.71238898038469,
+          "target": "3-street-view-west4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-street-view-west4",
+      "name": "Street View west4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6438152036640519,
+        "pitch": 0.19773863569870898,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5815208591150007,
+          "pitch": 0.18522353217347565,
+          "rotation": 1.5707963267948966,
+          "target": "2-street-view-west-3"
+        },
+        {
+          "yaw": 1.4386483304391708,
+          "pitch": 0.1833788258476332,
+          "rotation": 4.71238898038469,
+          "target": "3-street-view-west4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-street-view-west5",
+      "name": "Street View west5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5427647034661707,
+        "pitch": 0.21525978063404594,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3110251461839209,
+          "pitch": 0.224357128632942,
+          "rotation": 4.71238898038469,
+          "target": "3-street-view-west4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
